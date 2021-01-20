9b2a5b7d2f279d0ea4f5564b47ad6591fcd43b40
CosineMath <1365471105@qq.com>
2021-01-20 17:50:13 +0800

    Use new scan code to createKeyTables()

--- src/boat_init.c
+++ src/boat_init.c
@@ -35,184 +35,135 @@
 #include <unistd.h>
 
 
-// Translate the Boat key code for a key to a GLFW key code
-// NOTE: It is layout-dependent and will fail partially on most non-US layouts
-//
-static int translateKeyCode(int keycode)
-{
-    switch (keycode)
-    {
-        case BOAT_KEYBOARD_KP_0:           return GLFW_KEY_KP_0;
-        case BOAT_KEYBOARD_KP_1:           return GLFW_KEY_KP_1;
-        case BOAT_KEYBOARD_KP_2:           return GLFW_KEY_KP_2;
-        case BOAT_KEYBOARD_KP_3:           return GLFW_KEY_KP_3;
-        case BOAT_KEYBOARD_KP_4:           return GLFW_KEY_KP_4;
-        case BOAT_KEYBOARD_KP_5:           return GLFW_KEY_KP_5;
-        case BOAT_KEYBOARD_KP_6:           return GLFW_KEY_KP_6;
-        case BOAT_KEYBOARD_KP_7:           return GLFW_KEY_KP_7;
-        case BOAT_KEYBOARD_KP_8:           return GLFW_KEY_KP_8;
-        case BOAT_KEYBOARD_KP_9:           return GLFW_KEY_KP_9;
-        case BOAT_KEYBOARD_KP_Separator:
-        case BOAT_KEYBOARD_KP_Decimal:     return GLFW_KEY_KP_DECIMAL;
-        
-        case BOAT_KEYBOARD_Escape:         return GLFW_KEY_ESCAPE;
-        case BOAT_KEYBOARD_Tab:            return GLFW_KEY_TAB;
-        case BOAT_KEYBOARD_Shift_L:        return GLFW_KEY_LEFT_SHIFT;
-        case BOAT_KEYBOARD_Shift_R:        return GLFW_KEY_RIGHT_SHIFT;
-        case BOAT_KEYBOARD_Control_L:      return GLFW_KEY_LEFT_CONTROL;
-        case BOAT_KEYBOARD_Control_R:      return GLFW_KEY_RIGHT_CONTROL;
-        case BOAT_KEYBOARD_Meta_L:
-        case BOAT_KEYBOARD_Alt_L:          return GLFW_KEY_LEFT_ALT;
-        case BOAT_KEYBOARD_Mode_switch: // Mapped to Alt_R on many keyboards
-        case BOAT_KEYBOARD_ISO_Level3_Shift: // AltGr on at least some machines
-        case BOAT_KEYBOARD_Meta_R:
-        case BOAT_KEYBOARD_Alt_R:          return GLFW_KEY_RIGHT_ALT;
-        case BOAT_KEYBOARD_Super_L:        return GLFW_KEY_LEFT_SUPER;
-        case BOAT_KEYBOARD_Super_R:        return GLFW_KEY_RIGHT_SUPER;
-        case BOAT_KEYBOARD_Menu:           return GLFW_KEY_MENU;
-        case BOAT_KEYBOARD_Num_Lock:       return GLFW_KEY_NUM_LOCK;
-        case BOAT_KEYBOARD_Caps_Lock:      return GLFW_KEY_CAPS_LOCK;
-        case BOAT_KEYBOARD_Print:          return GLFW_KEY_PRINT_SCREEN;
-        case BOAT_KEYBOARD_Scroll_Lock:    return GLFW_KEY_SCROLL_LOCK;
-        case BOAT_KEYBOARD_Pause:          return GLFW_KEY_PAUSE;
-        case BOAT_KEYBOARD_Delete:         return GLFW_KEY_DELETE;
-        case BOAT_KEYBOARD_BackSpace:      return GLFW_KEY_BACKSPACE;
-        case BOAT_KEYBOARD_Return:         return GLFW_KEY_ENTER;
-        case BOAT_KEYBOARD_Home:           return GLFW_KEY_HOME;
-        case BOAT_KEYBOARD_End:            return GLFW_KEY_END;
-        case BOAT_KEYBOARD_Page_Up:        return GLFW_KEY_PAGE_UP;
-        case BOAT_KEYBOARD_Page_Down:      return GLFW_KEY_PAGE_DOWN;
-        case BOAT_KEYBOARD_Insert:         return GLFW_KEY_INSERT;
-        case BOAT_KEYBOARD_Left:           return GLFW_KEY_LEFT;
-        case BOAT_KEYBOARD_Right:          return GLFW_KEY_RIGHT;
-        case BOAT_KEYBOARD_Down:           return GLFW_KEY_DOWN;
-        case BOAT_KEYBOARD_Up:             return GLFW_KEY_UP;
-        case BOAT_KEYBOARD_F1:             return GLFW_KEY_F1;
-        case BOAT_KEYBOARD_F2:             return GLFW_KEY_F2;
-        case BOAT_KEYBOARD_F3:             return GLFW_KEY_F3;
-        case BOAT_KEYBOARD_F4:             return GLFW_KEY_F4;
-        case BOAT_KEYBOARD_F5:             return GLFW_KEY_F5;
-        case BOAT_KEYBOARD_F6:             return GLFW_KEY_F6;
-        case BOAT_KEYBOARD_F7:             return GLFW_KEY_F7;
-        case BOAT_KEYBOARD_F8:             return GLFW_KEY_F8;
-        case BOAT_KEYBOARD_F9:             return GLFW_KEY_F9;
-        case BOAT_KEYBOARD_F10:            return GLFW_KEY_F10;
-        case BOAT_KEYBOARD_F11:            return GLFW_KEY_F11;
-        case BOAT_KEYBOARD_F12:            return GLFW_KEY_F12;
-        case BOAT_KEYBOARD_F13:            return GLFW_KEY_F13;
-        case BOAT_KEYBOARD_F14:            return GLFW_KEY_F14;
-        case BOAT_KEYBOARD_F15:            return GLFW_KEY_F15;
-        case BOAT_KEYBOARD_F16:            return GLFW_KEY_F16;
-        case BOAT_KEYBOARD_F17:            return GLFW_KEY_F17;
-        case BOAT_KEYBOARD_F18:            return GLFW_KEY_F18;
-        case BOAT_KEYBOARD_F19:            return GLFW_KEY_F19;
-        case BOAT_KEYBOARD_F20:            return GLFW_KEY_F20;
-        case BOAT_KEYBOARD_F21:            return GLFW_KEY_F21;
-        case BOAT_KEYBOARD_F22:            return GLFW_KEY_F22;
-        case BOAT_KEYBOARD_F23:            return GLFW_KEY_F23;
-        case BOAT_KEYBOARD_F24:            return GLFW_KEY_F24;
-        case BOAT_KEYBOARD_F25:            return GLFW_KEY_F25;
-
-        // Numeric keypad
-        case BOAT_KEYBOARD_KP_Divide:      return GLFW_KEY_KP_DIVIDE;
-        case BOAT_KEYBOARD_KP_Multiply:    return GLFW_KEY_KP_MULTIPLY;
-        case BOAT_KEYBOARD_KP_Subtract:    return GLFW_KEY_KP_SUBTRACT;
-        case BOAT_KEYBOARD_KP_Add:         return GLFW_KEY_KP_ADD;
-
-        // These should have been detected in secondary keysym test above!
-        case BOAT_KEYBOARD_KP_Insert:      return GLFW_KEY_KP_0;
-        case BOAT_KEYBOARD_KP_End:         return GLFW_KEY_KP_1;
-        case BOAT_KEYBOARD_KP_Down:        return GLFW_KEY_KP_2;
-        case BOAT_KEYBOARD_KP_Page_Down:   return GLFW_KEY_KP_3;
-        case BOAT_KEYBOARD_KP_Left:        return GLFW_KEY_KP_4;
-        case BOAT_KEYBOARD_KP_Right:       return GLFW_KEY_KP_6;
-        case BOAT_KEYBOARD_KP_Home:        return GLFW_KEY_KP_7;
-        case BOAT_KEYBOARD_KP_Up:          return GLFW_KEY_KP_8;
-        case BOAT_KEYBOARD_KP_Page_Up:     return GLFW_KEY_KP_9;
-        case BOAT_KEYBOARD_KP_Delete:      return GLFW_KEY_KP_DECIMAL;
-        case BOAT_KEYBOARD_KP_Equal:       return GLFW_KEY_KP_EQUAL;
-        case BOAT_KEYBOARD_KP_Enter:       return GLFW_KEY_KP_ENTER;
-
-        // Last resort: Check for printable keys (should not happen if the XKB
-        // extension is available). This will give a layout dependent mapping
-        // (which is wrong, and we may miss some keys, especially on non-US
-        // keyboards), but it's better than nothing...
-        case BOAT_KEYBOARD_a:              return GLFW_KEY_A;
-        case BOAT_KEYBOARD_b:              return GLFW_KEY_B;
-        case BOAT_KEYBOARD_c:              return GLFW_KEY_C;
-        case BOAT_KEYBOARD_d:              return GLFW_KEY_D;
-        case BOAT_KEYBOARD_e:              return GLFW_KEY_E;
-        case BOAT_KEYBOARD_f:              return GLFW_KEY_F;
-        case BOAT_KEYBOARD_g:              return GLFW_KEY_G;
-        case BOAT_KEYBOARD_h:              return GLFW_KEY_H;
-        case BOAT_KEYBOARD_i:              return GLFW_KEY_I;
-        case BOAT_KEYBOARD_j:              return GLFW_KEY_J;
-        case BOAT_KEYBOARD_k:              return GLFW_KEY_K;
-        case BOAT_KEYBOARD_l:              return GLFW_KEY_L;
-        case BOAT_KEYBOARD_m:              return GLFW_KEY_M;
-        case BOAT_KEYBOARD_n:              return GLFW_KEY_N;
-        case BOAT_KEYBOARD_o:              return GLFW_KEY_O;
-        case BOAT_KEYBOARD_p:              return GLFW_KEY_P;
-        case BOAT_KEYBOARD_q:              return GLFW_KEY_Q;
-        case BOAT_KEYBOARD_r:              return GLFW_KEY_R;
-        case BOAT_KEYBOARD_s:              return GLFW_KEY_S;
-        case BOAT_KEYBOARD_t:              return GLFW_KEY_T;
-        case BOAT_KEYBOARD_u:              return GLFW_KEY_U;
-        case BOAT_KEYBOARD_v:              return GLFW_KEY_V;
-        case BOAT_KEYBOARD_w:              return GLFW_KEY_W;
-        case BOAT_KEYBOARD_x:              return GLFW_KEY_X;
-        case BOAT_KEYBOARD_y:              return GLFW_KEY_Y;
-        case BOAT_KEYBOARD_z:              return GLFW_KEY_Z;
-        case BOAT_KEYBOARD_1:              return GLFW_KEY_1;
-        case BOAT_KEYBOARD_2:              return GLFW_KEY_2;
-        case BOAT_KEYBOARD_3:              return GLFW_KEY_3;
-        case BOAT_KEYBOARD_4:              return GLFW_KEY_4;
-        case BOAT_KEYBOARD_5:              return GLFW_KEY_5;
-        case BOAT_KEYBOARD_6:              return GLFW_KEY_6;
-        case BOAT_KEYBOARD_7:              return GLFW_KEY_7;
-        case BOAT_KEYBOARD_8:              return GLFW_KEY_8;
-        case BOAT_KEYBOARD_9:              return GLFW_KEY_9;
-        case BOAT_KEYBOARD_0:              return GLFW_KEY_0;
-        case BOAT_KEYBOARD_space:          return GLFW_KEY_SPACE;
-        case BOAT_KEYBOARD_minus:          return GLFW_KEY_MINUS;
-        case BOAT_KEYBOARD_equal:          return GLFW_KEY_EQUAL;
-        case BOAT_KEYBOARD_bracketleft:    return GLFW_KEY_LEFT_BRACKET;
-        case BOAT_KEYBOARD_bracketright:   return GLFW_KEY_RIGHT_BRACKET;
-        case BOAT_KEYBOARD_backslash:      return GLFW_KEY_BACKSLASH;
-        case BOAT_KEYBOARD_semicolon:      return GLFW_KEY_SEMICOLON;
-        case BOAT_KEYBOARD_apostrophe:     return GLFW_KEY_APOSTROPHE;
-        case BOAT_KEYBOARD_grave:          return GLFW_KEY_GRAVE_ACCENT;
-        case BOAT_KEYBOARD_comma:          return GLFW_KEY_COMMA;
-        case BOAT_KEYBOARD_period:         return GLFW_KEY_PERIOD;
-        case BOAT_KEYBOARD_slash:          return GLFW_KEY_SLASH;
-        case BOAT_KEYBOARD_less:           return GLFW_KEY_WORLD_1; // At least in some layouts...
-        default:                break;
-    }
-
-    // No matching translation was found
-    return GLFW_KEY_UNKNOWN;
-}
-
 // Create key code translation tables
 //
 static void createKeyTables(void)
 {
-    int scancode, scancodeMin, scancodeMax;
+    int scancode;
 
     memset(_glfw.boat.keycodes, -1, sizeof(_glfw.boat.keycodes));
     memset(_glfw.boat.scancodes, -1, sizeof(_glfw.boat.scancodes));
 
-    scancodeMin = 0; 
-    scancodeMax = 255;
-    for (scancode = scancodeMin;  scancode <= scancodeMax;  scancode++)
-    {
-        // Translate the un-translated key codes using traditional Boat KeySym
-        // lookups
-        if (_glfw.boat.keycodes[scancode] < 0)
-        {
-            _glfw.boat.keycodes[scancode] = translateKeyCode(scancode);
-        }
+    _glfw.boat.keycodes[BOAT_KEY_GRAVE]      = GLFW_KEY_GRAVE_ACCENT;
+    _glfw.boat.keycodes[BOAT_KEY_1]          = GLFW_KEY_1;
+    _glfw.boat.keycodes[BOAT_KEY_2]          = GLFW_KEY_2;
+    _glfw.boat.keycodes[BOAT_KEY_3]          = GLFW_KEY_3;
+    _glfw.boat.keycodes[BOAT_KEY_4]          = GLFW_KEY_4;
+    _glfw.boat.keycodes[BOAT_KEY_5]          = GLFW_KEY_5;
+    _glfw.boat.keycodes[BOAT_KEY_6]          = GLFW_KEY_6;
+    _glfw.boat.keycodes[BOAT_KEY_7]          = GLFW_KEY_7;
+    _glfw.boat.keycodes[BOAT_KEY_8]          = GLFW_KEY_8;
+    _glfw.boat.keycodes[BOAT_KEY_9]          = GLFW_KEY_9;
+    _glfw.boat.keycodes[BOAT_KEY_0]          = GLFW_KEY_0;
+    _glfw.boat.keycodes[BOAT_KEY_SPACE]      = GLFW_KEY_SPACE;
+    _glfw.boat.keycodes[BOAT_KEY_MINUS]      = GLFW_KEY_MINUS;
+    _glfw.boat.keycodes[BOAT_KEY_EQUAL]      = GLFW_KEY_EQUAL;
+    _glfw.boat.keycodes[BOAT_KEY_Q]          = GLFW_KEY_Q;
+    _glfw.boat.keycodes[BOAT_KEY_W]          = GLFW_KEY_W;
+    _glfw.boat.keycodes[BOAT_KEY_E]          = GLFW_KEY_E;
+    _glfw.boat.keycodes[BOAT_KEY_R]          = GLFW_KEY_R;
+    _glfw.boat.keycodes[BOAT_KEY_T]          = GLFW_KEY_T;
+    _glfw.boat.keycodes[BOAT_KEY_Y]          = GLFW_KEY_Y;
+    _glfw.boat.keycodes[BOAT_KEY_U]          = GLFW_KEY_U;
+    _glfw.boat.keycodes[BOAT_KEY_I]          = GLFW_KEY_I;
+    _glfw.boat.keycodes[BOAT_KEY_O]          = GLFW_KEY_O;
+    _glfw.boat.keycodes[BOAT_KEY_P]          = GLFW_KEY_P;
+    _glfw.boat.keycodes[BOAT_KEY_LEFTBRACE]  = GLFW_KEY_LEFT_BRACKET;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHTBRACE] = GLFW_KEY_RIGHT_BRACKET;
+    _glfw.boat.keycodes[BOAT_KEY_A]          = GLFW_KEY_A;
+    _glfw.boat.keycodes[BOAT_KEY_S]          = GLFW_KEY_S;
+    _glfw.boat.keycodes[BOAT_KEY_D]          = GLFW_KEY_D;
+    _glfw.boat.keycodes[BOAT_KEY_F]          = GLFW_KEY_F;
+    _glfw.boat.keycodes[BOAT_KEY_G]          = GLFW_KEY_G;
+    _glfw.boat.keycodes[BOAT_KEY_H]          = GLFW_KEY_H;
+    _glfw.boat.keycodes[BOAT_KEY_J]          = GLFW_KEY_J;
+    _glfw.boat.keycodes[BOAT_KEY_K]          = GLFW_KEY_K;
+    _glfw.boat.keycodes[BOAT_KEY_L]          = GLFW_KEY_L;
+    _glfw.boat.keycodes[BOAT_KEY_SEMICOLON]  = GLFW_KEY_SEMICOLON;
+    _glfw.boat.keycodes[BOAT_KEY_APOSTROPHE] = GLFW_KEY_APOSTROPHE;
+    _glfw.boat.keycodes[BOAT_KEY_Z]          = GLFW_KEY_Z;
+    _glfw.boat.keycodes[BOAT_KEY_X]          = GLFW_KEY_X;
+    _glfw.boat.keycodes[BOAT_KEY_C]          = GLFW_KEY_C;
+    _glfw.boat.keycodes[BOAT_KEY_V]          = GLFW_KEY_V;
+    _glfw.boat.keycodes[BOAT_KEY_B]          = GLFW_KEY_B;
+    _glfw.boat.keycodes[BOAT_KEY_N]          = GLFW_KEY_N;
+    _glfw.boat.keycodes[BOAT_KEY_M]          = GLFW_KEY_M;
+    _glfw.boat.keycodes[BOAT_KEY_COMMA]      = GLFW_KEY_COMMA;
+    _glfw.boat.keycodes[BOAT_KEY_DOT]        = GLFW_KEY_PERIOD;
+    _glfw.boat.keycodes[BOAT_KEY_SLASH]      = GLFW_KEY_SLASH;
+    _glfw.boat.keycodes[BOAT_KEY_BACKSLASH]  = GLFW_KEY_BACKSLASH;
+    _glfw.boat.keycodes[BOAT_KEY_ESC]        = GLFW_KEY_ESCAPE;
+    _glfw.boat.keycodes[BOAT_KEY_TAB]        = GLFW_KEY_TAB;
+    _glfw.boat.keycodes[BOAT_KEY_LEFTSHIFT]  = GLFW_KEY_LEFT_SHIFT;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHTSHIFT] = GLFW_KEY_RIGHT_SHIFT;
+    _glfw.boat.keycodes[BOAT_KEY_LEFTCTRL]   = GLFW_KEY_LEFT_CONTROL;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHTCTRL]  = GLFW_KEY_RIGHT_CONTROL;
+    _glfw.boat.keycodes[BOAT_KEY_LEFTALT]    = GLFW_KEY_LEFT_ALT;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHTALT]   = GLFW_KEY_RIGHT_ALT;
+    _glfw.boat.keycodes[BOAT_KEY_LEFTMETA]   = GLFW_KEY_LEFT_SUPER;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHTMETA]  = GLFW_KEY_RIGHT_SUPER;
+    _glfw.boat.keycodes[BOAT_KEY_MENU]       = GLFW_KEY_MENU;
+    _glfw.boat.keycodes[BOAT_KEY_NUMLOCK]    = GLFW_KEY_NUM_LOCK;
+    _glfw.boat.keycodes[BOAT_KEY_CAPSLOCK]   = GLFW_KEY_CAPS_LOCK;
+    _glfw.boat.keycodes[BOAT_KEY_PRINT]      = GLFW_KEY_PRINT_SCREEN;
+    _glfw.boat.keycodes[BOAT_KEY_SCROLLLOCK] = GLFW_KEY_SCROLL_LOCK;
+    _glfw.boat.keycodes[BOAT_KEY_PAUSE]      = GLFW_KEY_PAUSE;
+    _glfw.boat.keycodes[BOAT_KEY_DELETE]     = GLFW_KEY_DELETE;
+    _glfw.boat.keycodes[BOAT_KEY_BACKSPACE]  = GLFW_KEY_BACKSPACE;
+    _glfw.boat.keycodes[BOAT_KEY_ENTER]      = GLFW_KEY_ENTER;
+    _glfw.boat.keycodes[BOAT_KEY_HOME]       = GLFW_KEY_HOME;
+    _glfw.boat.keycodes[BOAT_KEY_END]        = GLFW_KEY_END;
+    _glfw.boat.keycodes[BOAT_KEY_PAGEUP]     = GLFW_KEY_PAGE_UP;
+    _glfw.boat.keycodes[BOAT_KEY_PAGEDOWN]   = GLFW_KEY_PAGE_DOWN;
+    _glfw.boat.keycodes[BOAT_KEY_INSERT]     = GLFW_KEY_INSERT;
+    _glfw.boat.keycodes[BOAT_KEY_LEFT]       = GLFW_KEY_LEFT;
+    _glfw.boat.keycodes[BOAT_KEY_RIGHT]      = GLFW_KEY_RIGHT;
+    _glfw.boat.keycodes[BOAT_KEY_DOWN]       = GLFW_KEY_DOWN;
+    _glfw.boat.keycodes[BOAT_KEY_UP]         = GLFW_KEY_UP;
+    _glfw.boat.keycodes[BOAT_KEY_F1]         = GLFW_KEY_F1;
+    _glfw.boat.keycodes[BOAT_KEY_F2]         = GLFW_KEY_F2;
+    _glfw.boat.keycodes[BOAT_KEY_F3]         = GLFW_KEY_F3;
+    _glfw.boat.keycodes[BOAT_KEY_F4]         = GLFW_KEY_F4;
+    _glfw.boat.keycodes[BOAT_KEY_F5]         = GLFW_KEY_F5;
+    _glfw.boat.keycodes[BOAT_KEY_F6]         = GLFW_KEY_F6;
+    _glfw.boat.keycodes[BOAT_KEY_F7]         = GLFW_KEY_F7;
+    _glfw.boat.keycodes[BOAT_KEY_F8]         = GLFW_KEY_F8;
+    _glfw.boat.keycodes[BOAT_KEY_F9]         = GLFW_KEY_F9;
+    _glfw.boat.keycodes[BOAT_KEY_F10]        = GLFW_KEY_F10;
+    _glfw.boat.keycodes[BOAT_KEY_F11]        = GLFW_KEY_F11;
+    _glfw.boat.keycodes[BOAT_KEY_F12]        = GLFW_KEY_F12;
+    _glfw.boat.keycodes[BOAT_KEY_F13]        = GLFW_KEY_F13;
+    _glfw.boat.keycodes[BOAT_KEY_F14]        = GLFW_KEY_F14;
+    _glfw.boat.keycodes[BOAT_KEY_F15]        = GLFW_KEY_F15;
+    _glfw.boat.keycodes[BOAT_KEY_F16]        = GLFW_KEY_F16;
+    _glfw.boat.keycodes[BOAT_KEY_F17]        = GLFW_KEY_F17;
+    _glfw.boat.keycodes[BOAT_KEY_F18]        = GLFW_KEY_F18;
+    _glfw.boat.keycodes[BOAT_KEY_F19]        = GLFW_KEY_F19;
+    _glfw.boat.keycodes[BOAT_KEY_F20]        = GLFW_KEY_F20;
+    _glfw.boat.keycodes[BOAT_KEY_F21]        = GLFW_KEY_F21;
+    _glfw.boat.keycodes[BOAT_KEY_F22]        = GLFW_KEY_F22;
+    _glfw.boat.keycodes[BOAT_KEY_F23]        = GLFW_KEY_F23;
+    _glfw.boat.keycodes[BOAT_KEY_F24]        = GLFW_KEY_F24;
+    _glfw.boat.keycodes[BOAT_KEY_KPSLASH]    = GLFW_KEY_KP_DIVIDE;
+    _glfw.boat.keycodes[BOAT_KEY_KPDOT]      = GLFW_KEY_KP_MULTIPLY;
+    _glfw.boat.keycodes[BOAT_KEY_KPMINUS]    = GLFW_KEY_KP_SUBTRACT;
+    _glfw.boat.keycodes[BOAT_KEY_KPPLUS]     = GLFW_KEY_KP_ADD;
+    _glfw.boat.keycodes[BOAT_KEY_KP0]        = GLFW_KEY_KP_0;
+    _glfw.boat.keycodes[BOAT_KEY_KP1]        = GLFW_KEY_KP_1;
+    _glfw.boat.keycodes[BOAT_KEY_KP2]        = GLFW_KEY_KP_2;
+    _glfw.boat.keycodes[BOAT_KEY_KP3]        = GLFW_KEY_KP_3;
+    _glfw.boat.keycodes[BOAT_KEY_KP4]        = GLFW_KEY_KP_4;
+    _glfw.boat.keycodes[BOAT_KEY_KP5]        = GLFW_KEY_KP_5;
+    _glfw.boat.keycodes[BOAT_KEY_KP6]        = GLFW_KEY_KP_6;
+    _glfw.boat.keycodes[BOAT_KEY_KP7]        = GLFW_KEY_KP_7;
+    _glfw.boat.keycodes[BOAT_KEY_KP8]        = GLFW_KEY_KP_8;
+    _glfw.boat.keycodes[BOAT_KEY_KP9]        = GLFW_KEY_KP_9;
+    _glfw.boat.keycodes[BOAT_KEY_KPCOMMA]    = GLFW_KEY_KP_DECIMAL;
+    _glfw.boat.keycodes[BOAT_KEY_KPEQUAL]    = GLFW_KEY_KP_EQUAL;
+    _glfw.boat.keycodes[BOAT_KEY_KPENTER]    = GLFW_KEY_KP_ENTER;
 
+    for (scancode = BOAT_MIN_SCANCODE;  scancode <= BOAT_MAX_SCANCODE;  scancode++)
+    {
         // Store the reverse translation for faster key name lookup
         if (_glfw.boat.keycodes[scancode] > 0)
             _glfw.boat.scancodes[_glfw.boat.keycodes[scancode]] = scancode;
@@ -290,7 +241,7 @@ int _glfwPlatformInit(void)
     // EGL_DEFAULT_DISPLAY = 0.
     _glfw.boat.display = boatGetNativeDisplay();
     
-    //_glfw.boat.screen = 0;
+    _glfw.boat.screen = 0;
 
     getSystemContentScale(&_glfw.boat.contentScaleX, &_glfw.boat.contentScaleY);
 
